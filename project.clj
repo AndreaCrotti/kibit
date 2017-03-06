e46fa34a2e0466d35ce8e61d0b5f82094b8cab6a
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-03-06 14:26:02 +0000

    add rewrite-clj to try out actually rewriting the code

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
             :comments "Contact if any questions"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.logic "0.8.11"]
-                 [org.clojure/tools.cli "0.3.5"]]
+                 [org.clojure/tools.cli "0.3.5"]
+                 ;; could potentially add the rewrite-cljs as well?
+                 [rewrite-clj "0.6.0"]]
   :profiles {:dev {:dependencies [[lein-marginalia "0.9.0"]]
                    :resource-paths ["test/resources"]}}
   :deploy-repositories [["releases" :clojars]]
